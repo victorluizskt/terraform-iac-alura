dc4d4d9ae59bbf4c3c1fe869e6b907b3dc37a47b
victor <victorluizcefet@gmail.com>
2023-04-07 16:10:44 -0300

    add output public_ip in dev and prd

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -4,4 +4,9 @@ module "aws-dev" {
   region_us_east_1_aws = "us-east-1"
   key = "IaC-DEV"
   name_instance = "dev"
+  geral_access = "geral_access_dev"
+}
+
+output "public_ip_dev" {
+  value = module.aws-dev.public_ip
 }
--- env/prd/main.tf
+++ env/prd/main.tf
@@ -4,4 +4,9 @@ module "aws-prod" {
   region_us_east_1_aws = "us-east-1"
   key = "Iac-Prod"
   name_instance = "prd"
+  geral_access = "geral_access_prd"
+}
+
+output "public_ip_prd" {
+  value = module.aws-prod.public_ip
 }
