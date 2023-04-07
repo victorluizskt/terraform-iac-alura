8b09143882d21a77b488222bf276ca4862f298a6
victor <victorluizcefet@gmail.com>
2023-04-07 15:42:30 -0300

    alter locale main.tf, hosts.yml and playbook.ylm

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region_us_east_1_aws
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-006e00d6ac75d2ebb"
+  instance_type = var.instance
+  key_name = var.key
+}
+
+resource "aws_key_pair" "keySSH" {
+  key_name = var.key
+  public_key = file("${var.key}.pub")
+}
