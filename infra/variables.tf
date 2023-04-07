d6db84cbca36ab6c49502ac34ca6cf9ccdb9cb01
victor <victorluizcefet@gmail.com>
2023-04-07 15:40:26 -0300

    create variables

---
+++ infra/variables.tf
@@ -0,0 +1,13 @@
+# we do not need to assign the values ​​of the variables because in each environment we will have different values
+
+variable "region_us_east_1_aws" {
+    type = string
+}
+
+variable "key" {
+  type = string
+}
+
+variable "instance" {
+  type = string
+}
